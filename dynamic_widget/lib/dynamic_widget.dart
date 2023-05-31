42228dbc5f7b634471c35b24ccdbf4ac43014571
zpdl9089 <zpdl9089@el-electric.co.kr>
2023-05-31 18:28:03 +0900

    extension

--- dynamic_widget/lib/dynamic_widget.dart
+++ dynamic_widget/lib/dynamic_widget.dart
@@ -1,5 +1,11 @@
 library dynamic_widget;
 
+export 'package:dynamic_widget/src/extension/enum.dart';
+export 'package:dynamic_widget/src/extension/iterable.dart';
+export 'package:dynamic_widget/src/extension/list.dart';
+
 export 'package:dynamic_widget/src/style/text_style.dart';
 
 export 'package:dynamic_widget/src/util/shared_preferences_key.dart';
+
+export 'package:dynamic_widget/src/widget/overflow_widget.dart';
