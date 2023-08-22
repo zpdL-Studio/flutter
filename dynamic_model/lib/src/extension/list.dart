fc36dadd05c73aa43aa914059b4001ec5ea2754e
zpdl9089 <zpdl9089@el-electric.co.kr>
2023-08-22 15:45:17 +0900

    dynamic map list

--- dynamic_model/lib/src/extension/list.dart
+++ dynamic_model/lib/src/extension/list.dart
@@ -37,4 +37,49 @@ extension ListExtension<E> on List<E> {
     }
     return results;
   }
+
+  Map<K, E> dynamicToMap<K>(K Function(E element) valueToKey) {
+    final map = <K, E>{};
+    for (final element in this) {
+      map[valueToKey(element)] = element;
+    }
+    return map;
+  }
+}
+
+class DynamicMapList<K, V> {
+  Map<K, V>? _map;
+  final List<V> list;
+  final K Function(V element) valueToKey;
+
+  DynamicMapList({required this.list, required this.valueToKey});
+
+  Map<K, V> get map {
+    _map ??= list.dynamicToMap(valueToKey);
+    return _map!;
+  }
+
+  void add(V item) {
+    _map = null;
+    list.add(item);
+  }
+
+  void addAll(Iterable<V> iterable) {
+    _map = null;
+    list.addAll(iterable);
+  }
+
+  bool remove(Object? value) {
+    _map = null;
+    return list.remove(value);
+  }
+
+  V removeAt(int index) {
+    _map = null;
+    return list.removeAt(index);
+  }
+
+  bool containsKey(K key) {
+    return map.containsKey(key);
+  }
 }
