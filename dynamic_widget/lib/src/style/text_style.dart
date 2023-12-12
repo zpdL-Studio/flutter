0a73d9d0688d806dc8554c664fa91091aa45c7d8
zpdl9089 <zpdl9089@el-electric.co.kr>
2023-12-12 11:04:40 +0900

    text_style

--- dynamic_widget/lib/src/style/text_style.dart
+++ dynamic_widget/lib/src/style/text_style.dart
@@ -200,13 +200,13 @@ extension DynamicTextTextStyleByColor on Color {
   }
 
   TextStyle semiBold(
-      double fontSize, {
-        FontStyle? fontStyle,
-        TextDecoration? decoration,
-        Color? decorationColor,
-        TextDecorationStyle? decorationStyle,
-        double? decorationThickness,
-      }) {
+    double fontSize, {
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+  }) {
     return w600(
       fontSize,
       fontStyle: fontStyle,
@@ -246,4 +246,195 @@ extension DynamicTextTextStyleByTheme on ThemeData {
   Color get tertiaryTextColor => colorScheme.onTertiary;
 
   Color get errorTextColor => colorScheme.onError;
+
+  TextStyle get defaultTextStyle => textTheme.bodyMedium ?? const TextStyle();
+
+  TextStyle w100(
+    double fontSize, {
+    Color? color,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+  }) {
+    return defaultTextStyle.copyWith(
+      fontSize: fontSize,
+      fontStyle: fontStyle,
+      fontWeight: FontWeight.w100,
+      color: color,
+      decoration: decoration,
+      decorationColor: decorationColor,
+      decorationStyle: decorationStyle,
+      decorationThickness: decorationThickness,
+    );
+  }
+
+  TextStyle w200(
+    double fontSize, {
+    Color? color,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+  }) {
+    return defaultTextStyle.copyWith(
+      fontSize: fontSize,
+      fontStyle: fontStyle,
+      fontWeight: FontWeight.w200,
+      color: color,
+      decoration: decoration,
+      decorationColor: decorationColor,
+      decorationStyle: decorationStyle,
+      decorationThickness: decorationThickness,
+    );
+  }
+
+  TextStyle w300(
+    double fontSize, {
+    Color? color,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+  }) {
+    return defaultTextStyle.copyWith(
+      fontSize: fontSize,
+      fontStyle: fontStyle,
+      fontWeight: FontWeight.w300,
+      color: color,
+      decoration: decoration,
+      decorationColor: decorationColor,
+      decorationStyle: decorationStyle,
+      decorationThickness: decorationThickness,
+    );
+  }
+
+  TextStyle w400(
+    double fontSize, {
+    Color? color,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+  }) {
+    return defaultTextStyle.copyWith(
+      fontSize: fontSize,
+      fontStyle: fontStyle,
+      fontWeight: FontWeight.w400,
+      color: color,
+      decoration: decoration,
+      decorationColor: decorationColor,
+      decorationStyle: decorationStyle,
+      decorationThickness: decorationThickness,
+    );
+  }
+
+  TextStyle w500(
+    double fontSize, {
+    Color? color,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+  }) {
+    return defaultTextStyle.copyWith(
+      fontSize: fontSize,
+      fontStyle: fontStyle,
+      fontWeight: FontWeight.w500,
+      color: color,
+      decoration: decoration,
+      decorationColor: decorationColor,
+      decorationStyle: decorationStyle,
+      decorationThickness: decorationThickness,
+    );
+  }
+
+  TextStyle w600(
+    double fontSize, {
+    Color? color,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+  }) {
+    return defaultTextStyle.copyWith(
+      fontSize: fontSize,
+      fontStyle: fontStyle,
+      fontWeight: FontWeight.w600,
+      color: color,
+      decoration: decoration,
+      decorationColor: decorationColor,
+      decorationStyle: decorationStyle,
+      decorationThickness: decorationThickness,
+    );
+  }
+
+  TextStyle w700(
+    double fontSize, {
+    Color? color,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+  }) {
+    return defaultTextStyle.copyWith(
+      fontSize: fontSize,
+      fontStyle: fontStyle,
+      fontWeight: FontWeight.w700,
+      color: color,
+      decoration: decoration,
+      decorationColor: decorationColor,
+      decorationStyle: decorationStyle,
+      decorationThickness: decorationThickness,
+    );
+  }
+
+  TextStyle w800(
+    double fontSize, {
+    Color? color,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+  }) {
+    return defaultTextStyle.copyWith(
+      fontSize: fontSize,
+      fontStyle: fontStyle,
+      fontWeight: FontWeight.w800,
+      color: color,
+      decoration: decoration,
+      decorationColor: decorationColor,
+      decorationStyle: decorationStyle,
+      decorationThickness: decorationThickness,
+    );
+  }
+
+  TextStyle w900(
+    double fontSize, {
+    Color? color,
+    FontStyle? fontStyle,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+  }) {
+    return defaultTextStyle.copyWith(
+      fontSize: fontSize,
+      fontStyle: fontStyle,
+      fontWeight: FontWeight.w900,
+      color: color,
+      decoration: decoration,
+      decorationColor: decorationColor,
+      decorationStyle: decorationStyle,
+      decorationThickness: decorationThickness,
+    );
+  }
 }
