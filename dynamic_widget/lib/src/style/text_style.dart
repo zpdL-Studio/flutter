f93f929e31887ef4c1ca057b8be7fd825a65ba7a
zpdl9089 <zpdl9089@gmail.com>
2023-12-16 11:48:12 +0900

    text style

--- dynamic_widget/lib/src/style/text_style.dart
+++ dynamic_widget/lib/src/style/text_style.dart
@@ -1,240 +1,240 @@
 import 'package:flutter/material.dart';
 
-extension DynamicTextTextStyleByColor on Color {
-  TextStyle w100(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return TextStyle(
-      fontSize: fontSize,
-      fontStyle: fontStyle,
-      fontWeight: FontWeight.w100,
-      color: this,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle w200(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return TextStyle(
-      fontSize: fontSize,
-      fontStyle: fontStyle,
-      fontWeight: FontWeight.w200,
-      color: this,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle w300(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return TextStyle(
-      fontSize: fontSize,
-      fontStyle: fontStyle,
-      fontWeight: FontWeight.w300,
-      color: this,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle w400(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return TextStyle(
-      fontSize: fontSize,
-      fontStyle: fontStyle,
-      fontWeight: FontWeight.w400,
-      color: this,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle w500(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return TextStyle(
-      fontSize: fontSize,
-      fontStyle: fontStyle,
-      fontWeight: FontWeight.w500,
-      color: this,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle w600(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return TextStyle(
-      fontSize: fontSize,
-      fontStyle: fontStyle,
-      fontWeight: FontWeight.w600,
-      color: this,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle w700(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return TextStyle(
-      fontSize: fontSize,
-      fontStyle: fontStyle,
-      fontWeight: FontWeight.w700,
-      color: this,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle w800(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return TextStyle(
-      fontSize: fontSize,
-      fontStyle: fontStyle,
-      fontWeight: FontWeight.w800,
-      color: this,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle w900(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return TextStyle(
-      fontSize: fontSize,
-      fontStyle: fontStyle,
-      fontWeight: FontWeight.w900,
-      color: this,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle normal(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return w400(
-      fontSize,
-      fontStyle: fontStyle,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle semiBold(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return w600(
-      fontSize,
-      fontStyle: fontStyle,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-
-  TextStyle bold(
-    double fontSize, {
-    FontStyle? fontStyle,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) {
-    return w700(
-      fontSize,
-      fontStyle: fontStyle,
-      decoration: decoration,
-      decorationColor: decorationColor,
-      decorationStyle: decorationStyle,
-      decorationThickness: decorationThickness,
-    );
-  }
-}
+// extension DynamicTextTextStyleByColor on Color {
+//   TextStyle w100(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return TextStyle(
+//       fontSize: fontSize,
+//       fontStyle: fontStyle,
+//       fontWeight: FontWeight.w100,
+//       color: this,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle w200(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return TextStyle(
+//       fontSize: fontSize,
+//       fontStyle: fontStyle,
+//       fontWeight: FontWeight.w200,
+//       color: this,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle w300(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return TextStyle(
+//       fontSize: fontSize,
+//       fontStyle: fontStyle,
+//       fontWeight: FontWeight.w300,
+//       color: this,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle w400(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return TextStyle(
+//       fontSize: fontSize,
+//       fontStyle: fontStyle,
+//       fontWeight: FontWeight.w400,
+//       color: this,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle w500(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return TextStyle(
+//       fontSize: fontSize,
+//       fontStyle: fontStyle,
+//       fontWeight: FontWeight.w500,
+//       color: this,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle w600(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return TextStyle(
+//       fontSize: fontSize,
+//       fontStyle: fontStyle,
+//       fontWeight: FontWeight.w600,
+//       color: this,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle w700(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return TextStyle(
+//       fontSize: fontSize,
+//       fontStyle: fontStyle,
+//       fontWeight: FontWeight.w700,
+//       color: this,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle w800(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return TextStyle(
+//       fontSize: fontSize,
+//       fontStyle: fontStyle,
+//       fontWeight: FontWeight.w800,
+//       color: this,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle w900(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return TextStyle(
+//       fontSize: fontSize,
+//       fontStyle: fontStyle,
+//       fontWeight: FontWeight.w900,
+//       color: this,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle normal(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return w400(
+//       fontSize,
+//       fontStyle: fontStyle,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle semiBold(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return w600(
+//       fontSize,
+//       fontStyle: fontStyle,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+//
+//   TextStyle bold(
+//     double fontSize, {
+//     FontStyle? fontStyle,
+//     TextDecoration? decoration,
+//     Color? decorationColor,
+//     TextDecorationStyle? decorationStyle,
+//     double? decorationThickness,
+//   }) {
+//     return w700(
+//       fontSize,
+//       fontStyle: fontStyle,
+//       decoration: decoration,
+//       decorationColor: decorationColor,
+//       decorationStyle: decorationStyle,
+//       decorationThickness: decorationThickness,
+//     );
+//   }
+// }
 
 extension DynamicTextTextStyleByTheme on ThemeData {
   Color get textColor => colorScheme.onSurface;
@@ -246,6 +246,8 @@ extension DynamicTextTextStyleByTheme on ThemeData {
   Color get tertiaryTextColor => colorScheme.onTertiary;
 
   Color get errorTextColor => colorScheme.onError;
+
+  Color get outlineColor => colorScheme.outline;
 
   TextStyle get defaultTextStyle => textTheme.bodyMedium ?? const TextStyle();
 
